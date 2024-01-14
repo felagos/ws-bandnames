1660cb4d04bd5b3d06fdf9dce6c307f43e73ec45
Felipe Lagos <felagossanchez@gmail.com>
2024-01-14 17:58:18 -0300

    Refactor App component layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import io from "socket.io-client";
 
 import "./App.scss";
 
+
 const connectSocket = () => {
 	const socket = io("http://localhost:3000");
 	return socket;
@@ -14,7 +15,6 @@ const socket = connectSocket();
 
 export const App = () => {
 	const [isOnline, setIsOnline] = useState(socket.connected);
-
 
 	useEffect(() => {
 		socket.on('connect', () => {
@@ -31,11 +31,11 @@ export const App = () => {
 			<Status isOnline={isOnline} className="status" />
 			<h1>BandNames</h1>
 			<Divider plain />
-			<Row gutter={[24, 24]}>
-				<Col span={12}>
+			<Row>
+				<Col xl={12} sm={24} xs={24}>
 					<BandList />
 				</Col>
-				<Col span={12}>
+				<Col xl={12} sm={24} xs={24}>
 					<BandAdd />
 				</Col>
 			</Row>
